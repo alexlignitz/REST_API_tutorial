023d1d960f1a5dca43bc98cbf88b96d0293911b2
alexlignitz <alexlignitz@gmail.com>
2021-09-13 22:28:27 +0200

    Add api view

--- api_project/serializers.py
+++ api_project/serializers.py
@@ -5,5 +5,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id', 'title', 'author']
+        fields = '__all__'
 
