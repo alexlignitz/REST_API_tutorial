2705d09edb0b37c9da64974125c1315670b3abc9
alexlignitz <alexlignitz@gmail.com>
2021-09-14 09:12:26 +0200

    Add session and basic authentication

--- REST_API_tutorial/settings.py
+++ REST_API_tutorial/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api_project',
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
--- api_project/views.py
+++ api_project/views.py
@@ -1,13 +1,17 @@
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics, mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return self.list(request)
